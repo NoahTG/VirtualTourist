82077c9b0a17804f93f9fdaeb46d62b9a407c6d9
NoahTG <26232817+NoahTG@users.noreply.github.com>
2019-11-05 22:51:16 -0800

    Add Alert Extension

---
+++ VirtualTourist/AlertExtension.swift
@@ -0,0 +1,24 @@
+//
+//  AlertExtension.swift
+//  VirtualTourist
+//
+//  Created by NTG on 11/5/19.
+//  Copyright © 2019 NTG. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+   
+    func showAlert(title: String, message: String?){
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        DispatchQueue.main.async {
+            self.show(alertVC, sender: nil)
+        }
+    }
+    
+}
