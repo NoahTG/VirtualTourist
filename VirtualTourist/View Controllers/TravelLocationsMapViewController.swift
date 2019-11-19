838141c6ff53a167ca7b218daab52239981a85e4
NoahTG <26232817+NoahTG@users.noreply.github.com>
2019-11-18 22:24:20 -0800

    revise props in mapVC

--- VirtualTourist/View Controllers/TravelLocationsMapViewController.swift
+++ VirtualTourist/View Controllers/TravelLocationsMapViewController.swift
@@ -6,25 +6,44 @@
 //  Copyright © 2019 NTG. All rights reserved.
 //
 
-import Foundation
 import MapKit
 import CoreData
 import UIKit
 
-class TravelLocationsMapViewController: UIViewController, MKMapViewDelegate {
+class TravelLocationsMapViewController: UIViewController {
     
     //MARK: Properties
             
       // add property to hold data from persistence store
-      var dataController:DataController!
+    var dataController:DataController!
     
-      var pin:Pin!
+    // protocol for pulling photos from Flickr and populating album
+    var flickrClient: FlickrClientProtocol
     
-      var annotations = [MKPointAnnotation]()
+    // protocol for persisting new pin
+    var pinPersistence: PinPersitenceProtocol
+    
+    // protocol for persisting photo album associated with pin
+    var albumPersistence: AlbumPersistenceProtocol
+    
+   // MARK: Life Cycle
 
        override func viewDidLoad() {
-        
              super.viewDidLoad()
       }
     
+    
+    
+    
+    
+    
+    
+    
+    
 }
+
+
+
+extension
+
+var annotations = [MKPointAnnotation]()
